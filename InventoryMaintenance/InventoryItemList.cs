a74cbda0a8b4af4a06fa929c744b006e8fbbf8a7
bguzman07 <brandyguzman07@gmail.com>
2025-10-23 14:25:35 -0500

    first commit

---
+++ InventoryMaintenance/InventoryItemList.cs
@@ -0,0 +1,60 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace InventoryMaintenance
+{
+    public class InventoryItemList
+    {
+        private List<InventoryItem> items;
+
+        public void Add(InventoryItem item)
+        {
+            
+        }
+
+        public static InventoryItemList operator +(InventoryItem item, InventoryItemList list)
+        {
+             list.items.Add(item); 
+            return list;
+        }
+        public static InventoryItemList operator -(InventoryItem item, InventoryItemList list)
+        {
+            list.items.Remove(item);
+            return list;
+        }
+
+
+        public InventoryItemList()
+        {
+            items = new List<InventoryItem>();
+        }
+        public delegate void ChangeHandler(InventoryItemList list);
+
+        public event ChangeHandler changed;
+
+
+      
+     
+
+        public int Count => items.Count;
+
+        public InventoryItem this[int i]
+        {
+            get { return items[i]; }
+            set { items[i] = value; }
+        }
+
+
+
+        public void Add(int itemNo, string description, decimal price)
+        {
+            InventoryItem i = new(itemNo, description, price);
+            items.Add(i);
+        }
+
+        public void Remove(InventoryItem item) => items.Remove(item);
+
+        public void Fill() => items = InventoryDB.GetItems();
+
+        public void Save() => InventoryDB.SaveItems(items);
+    }
+}
